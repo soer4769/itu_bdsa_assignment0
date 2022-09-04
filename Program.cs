320a0c23f396be51139a2f54b6cc70b41545a8fd
Søren Kastrup <311363@RTS-365.dk>
2022-09-04 13:55:31 +0200

    Created user interface for IsLeapYear

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Enter a year to check for if a leap year: ");
+        string year = Console.ReadLine();
+        Console.WriteLine(IsLeapYear(int.Parse(year)) ? "Yay" : "Nay");
     }
 
-    public bool IsLeapYear(int year)
+    public static bool IsLeapYear(int year)
     {
         return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
     }
--- assignment0tests/UnitTest1.cs
+++ assignment0tests/UnitTest1.cs
@@ -2,33 +2,26 @@ namespace assignment0tests;
 
 public class UnitTest1
 {
-    private readonly Program _p;
-
-    public UnitTest1()
-    {
-        _p = new Program();
-    }
-
     [Fact]
     public void IsLeapYearDivisibleByFourTest()
     {
-        Assert.Equal(true,_p.IsLeapYear(2004));
-        Assert.Equal(false,_p.IsLeapYear(2005));
+        Assert.Equal(true,Program.IsLeapYear(2004));
+        Assert.Equal(false,Program.IsLeapYear(2005));
     }
 
     [Fact]
     public void IsLeapYearExceptDivisibleByHundredTest()
     {
-        Assert.Equal(false,_p.IsLeapYear(1700));
-        Assert.Equal(false,_p.IsLeapYear(1800));
-        Assert.Equal(false,_p.IsLeapYear(1900));
-        Assert.Equal(true,_p.IsLeapYear(2004));
+        Assert.Equal(false,Program.IsLeapYear(1700));
+        Assert.Equal(false,Program.IsLeapYear(1800));
+        Assert.Equal(false,Program.IsLeapYear(1900));
+        Assert.Equal(true,Program.IsLeapYear(2004));
     }
 
     [Fact]
     public void IsLeapYearExceptDivisibleByFourHundredTest()
     {
-        Assert.Equal(true,_p.IsLeapYear(1600));
-        Assert.Equal(true,_p.IsLeapYear(2000));
+        Assert.Equal(true,Program.IsLeapYear(1600));
+        Assert.Equal(true,Program.IsLeapYear(2000));
     }
 }
